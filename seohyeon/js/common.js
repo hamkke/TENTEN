05e99828eddf5de473519ec67f62ec29fe53f552
hamkke <walwalzz@naver.com>
2019-10-16 15:59:02 +0900

    완성완성

--- seohyeon/js/common.js
+++ seohyeon/js/common.js
@@ -1,19 +1,16 @@
-$ (document).ready(function(){
-
-
+$(document).ready(function(){
     
-    $(".gnb_list li",this).mouseenter(function(){
-        $('.lnb_group',this).stop().slideDown(100)
-
-        $('.lnb_group',this).mouseleave(function(){
-            $('.lnb_group').hide()
+    $('.gnb_list li').mouseover(function(){
+        $('.gnb_list li').mouseleave(function(){
+            $(".lnb_group").hide()
         })
-       
+        $(".lnb_group",this).stop().slideDown(100)
+        $('.lnb_group').mouseleave(function(){
+            $(".lnb_group").hide()
+        })
         
         
-    })
-    $('.gnb_list li',this).mouseleave(function(){
-        $('.lnb_group').hide()
+        
     })
     
     
